b563185371abb58c6bb38f9a24558a7f73f3af83
Brian <rac3k@aol.com>
2018-06-19 21:22:42 -0400

    api key added

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-
-
+//using giphy URL plus additional &rating &limit &apikey
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cartoon + "&rating=pg&api_key=oVUQJ87fbQBoc6RoeL6zt6GcCKcYvyPs&limit=13";
 
 
 
