25d496376254055bb44192fb4aa348937373c93e
Brian <rac3k@aol.com>
2018-06-20 22:37:14 -0400

    AJAX and display function

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,9 +1,19 @@
-$(document).ready(function() {
-//using giphy URL plus additional &rating &limit &apikey
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cartoon + "&rating=pg&api_key=oVUQJ87fbQBoc6RoeL6zt6GcCKcYvyPs&limit=13";
+$(document).ready(function () {
 
+        function display() {
+            var animal = $(this).attr("data-name");
+            //using giphy URL & apikey
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animals + "api_key=DfEDLoxtRsTuunAcM6Uinvl6dmatdLn6";
+            
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
 
+                var results = response.data;
 
+           });
+        }
 
 
 
