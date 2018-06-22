c8a8f4d7592ccb563543984a5df2f7198b031a7f
Brian <rac3k@aol.com>
2018-06-22 18:07:46 -0400

    added gif function

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,9 +1,32 @@
 $(document).ready(function () {
+    var animals = ["Dogs"];
+
+    function addButtons(){
+
+        $("#animalView").empty();
+        for (var i = 0; i < animals.length; i++) {
+            var a = $('<button>')
+            a.addClass("animal");
+            a.attr("data-name", animals[i]);
+            a.text(animals[i]);
+            $("#animalView").append(a);
+
+        }
+    }
+
+    $("#addAnimal").click(function() {
+        var animal = $("#animal-input").val().trim();
+        animals.push(animal);
+        addButtons();
+        return false;
+    });
+
 
         function display() {
+
             var animal = $(this).attr("data-name");
             //using giphy URL & apikey
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animals + "api_key=DfEDLoxtRsTuunAcM6Uinvl6dmatdLn6";
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animals + "&api_key=DfEDLoxtRsTuunAcM6Uinvl6dmatdLn6&limit=5";
             
             $.ajax({
                 url: queryURL,
@@ -11,14 +34,26 @@ $(document).ready(function () {
             }).then(function (response) {
 
                 var results = response.data;
+                for (var i =0; i < results.length; i++) {
+                    var newDiv = $("<div class=gif>");
+                    var gif = $("<img>");
+                        gif.attr("src", results[i].images.fixed_height.url);
+                        gif.addClass("gif")
+
+                    newDiv.append(gif)
+                    $("#images").prepend(newDiv);
+                }
 
            });
+
         }
 
+        $(document).click(".gif", function() {
 
+        });
 
+        $(document).click(".animal", display);
 
-
-    
+        addButtons();
 
 });
